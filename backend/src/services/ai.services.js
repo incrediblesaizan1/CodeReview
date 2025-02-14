29c69103f9027b98361c6e6a85b72d19c434deea
incrediblesaizan1 <glorioussaizan1@gmail.com>
2025-02-14 20:58:55 +0530

    c

--- backend/src/services/ai.services.js
+++ backend/src/services/ai.services.js
@@ -5,16 +5,7 @@ const model = genAI.getGenerativeModel({ model: "gemini-2.0-flash" });
 
  export const getData = async () => {
     const prompt = `
-    import express from "express";
-    const app = express();
-    
-    app.get("/", async(req, res) => {
-        const data = await getData();
-        res.send("QUESTION: How are you today? <br> Answer: " + data);
-    });
-
-    export default app;
-    Review this code.
+    how are you today
     `;
     const result = await model.generateContent(prompt);
     return result.response.text();
