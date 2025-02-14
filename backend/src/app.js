c350fe48fba9e88ca6e05f2a4880dbab08063dff
incrediblesaizan1 <glorioussaizan1@gmail.com>
2025-02-14 22:42:06 +0530

    changes

--- backend/src/app.js
+++ backend/src/app.js
@@ -2,9 +2,19 @@ import express from "express";
 const app = express();
 import {getData} from "./services/ai.services.js"
 
+
+app.use(express.json());
+
+
 app.get("/", async(req, res) => {
-    const data = await getData()
-    res.send(data)
+   res.send("hello")
 });
+
+app.post("/",async(req,res)=>{
+    const question = req.body.code
+    const data = await getData(question) 
+    res.send(data)
+})
+
 
 export default app;
--- backend/src/services/ai.services.js
+++ backend/src/services/ai.services.js
@@ -3,10 +3,8 @@ import { GoogleGenerativeAI } from "@google/generative-ai";
 const genAI = new GoogleGenerativeAI("AIzaSyAsRKRvBhE4em-MqsrHHyxVrQ1xe3D-toQ");
 const model = genAI.getGenerativeModel({ model: "gemini-2.0-flash" });
 
- export const getData = async () => {
-    const prompt = `
-    can you review my code
-    `;
+ export const getData = async (question = "ask me hello how are you") => {
+    const prompt = `${question}`
     const result = await model.generateContent(prompt);
     console.log(result.response.text())
     return result.response.text();
