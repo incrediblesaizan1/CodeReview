23a8bf48cd5dca4b0ee5e722e1f1a773b57cbc38
incrediblesaizan1 <glorioussaizan1@gmail.com>
2025-02-16 01:19:01 +0530

    changes

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,31 +1,60 @@
-import { useState } from 'react'
-import arrow from "../public/arrow.svg"
-import { axiosInstance } from './function/axiosInstance'
+import { useState } from "react";
+import arrow from "../public/arrow.svg";
+import { axiosInstance } from "./function/axiosInstance";
+import Loader2 from "./constant/Loader";
 
 function App() {
+  const [code, setCode] = useState("");
+  const [error, setError] = useState(false);
+  const [errorText, setErrorText] = useState("");
+  const [response, setResponse] = useState(String);
+  const [loading, setLoading] = useState(false);
 
-  const [code,setCode] = useState("")
-
-  const onsubmit = async (e: React.FormEvent<HTMLFormElement>) =>{
-    e.preventDefault()
- const res:string =  await axiosInstance.post("/",{
-    "code": code
-  })
-console.log(res)
-  }
-
+  const onsubmit = async (e: React.FormEvent<HTMLFormElement>) => {
+    e.preventDefault();
+    if (!code) {
+      setError(true);
+      return setErrorText("please enter some text ");
+    }
+    setLoading(true);
+    setError(false);
+    const res: string = await axiosInstance.post("/", {
+      code: code,
+    });
+    setResponse(res.data);
+    setLoading(false);
+  };
 
   return (
     <>
-    <div className='flex bg-[#242424] gap-4 p-4 h-screen'>
-      <form onSubmit={onsubmit} className='relative' >
-  <textarea value={code} onChange={(e)=>setCode(e.target.value)} name="code" className='w-[50vw] text-white rounded-2xl h-[95vh] p-4 pl-6 bg-[#0a0a0a] resize-none'></textarea>
-  <button type='submit' className=' cursor-pointer absolute bottom-4 right-4 text-white'><img src={arrow} className='bg-white rounded-3xl' alt="submit" /></button>
-      </form>
-  <div className='bg-[#343434] w-[50vw] rounded-2xl text-white p-4 pl-6'></div>
-    </div>
+      <div className="flex bg-[#242424] gap-4 p-4 h-full custom-scrollbar2">
+        <form onSubmit={onsubmit} className="relative ">
+          <textarea
+            value={code}
+            onChange={(e) => setCode(e.target.value)}
+            name="code"
+            className=" custom-scrollbar2 w-[50vw] text-white rounded-r h-[95vh] p-4 pl-6 bg-[#0a0a0a] resize-none"
+          ></textarea>
+          <button
+            type="submit"
+            className=" cursor-pointer absolute bottom-4 right-4 text-white"
+          >
+            <img src={arrow} className="bg-white rounded-3xl" alt="submit" />
+          </button>
+        </form>
+
+        <div className="bg-[#343434] w-[50vw] h-[95vh] overflow-auto custom-scrollbar2 rounded-r text-white p-4 pl-6">
+          {error ? (
+            <span className="text-red-700 text-center">{errorText}</span>
+          ) : loading ? (
+            <Loader2 />
+          ) : (
+            <pre className="whitespace-pre-wrap">{typeof response === "string"? response:JSON.stringify(response, null,2)}</pre>
+          )}
+        </div>
+      </div>
     </>
-  )
+  );
 }
 
-export default App
+export default App;
