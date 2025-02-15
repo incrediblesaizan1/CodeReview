8d129999774512cc11b84dfe82b1ebb65b91970e
incrediblesaizan1 <glorioussaizan1@gmail.com>
2025-02-16 01:48:19 +0530

    changes

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,60 +1,62 @@
-import { useState } from "react";
-import arrow from "../public/arrow.svg";
-import { axiosInstance } from "./function/axiosInstance";
-import Loader2 from "./constant/Loader";
+  import { useState } from "react";
+  import arrow from "../public/arrow.svg";
+  import { axiosInstance } from "./function/axiosInstance";
+  import Loader2 from "./constant/Loader";
 
-function App() {
-  const [code, setCode] = useState("");
-  const [error, setError] = useState(false);
-  const [errorText, setErrorText] = useState("");
-  const [response, setResponse] = useState(String);
-  const [loading, setLoading] = useState(false);
+  function App() {
+    const [logo, setLogo] = ("                  ")
+    const [code, setCode] = useState(logo);
+    const [error, setError] = useState(false);
+    const [errorText, setErrorText] = useState("");
+    const [response, setResponse] = useState(String);
+    const [loading, setLoading] = useState(false);
 
-  const onsubmit = async (e: React.FormEvent<HTMLFormElement>) => {
-    e.preventDefault();
-    if (!code) {
-      setError(true);
-      return setErrorText("please enter some text ");
-    }
-    setLoading(true);
-    setError(false);
-    const res: string = await axiosInstance.post("/", {
-      code: code,
-    });
-    setResponse(res.data);
-    setLoading(false);
-  };
+    const onsubmit = async (e: React.FormEvent<HTMLFormElement>) => {
+      e.preventDefault();
+      if (!code) {
+        setError(true);
+        return setErrorText("please enter some text ");
+      }
+      setLoading(true);
+      setError(false);
+      const res: string = await axiosInstance.post("/", {
+        code: code,
+      });
+      setResponse(res.data);
+      setLoading(false);
+    };
 
-  return (
-    <>
-      <div className="flex bg-[#242424] gap-4 p-4 h-full custom-scrollbar2">
-        <form onSubmit={onsubmit} className="relative ">
-          <textarea
-            value={code}
-            onChange={(e) => setCode(e.target.value)}
-            name="code"
-            className=" custom-scrollbar2 w-[50vw] text-white rounded-r h-[95vh] p-4 pl-6 bg-[#0a0a0a] resize-none"
-          ></textarea>
-          <button
-            type="submit"
-            className=" cursor-pointer absolute bottom-4 right-4 text-white"
-          >
-            <img src={arrow} className="bg-white rounded-3xl" alt="submit" />
-          </button>
-        </form>
+    return (
+      <>
+            <h1 className=" bg-[#242424]  absolute p-4 text-white z-40 text-2xl font-[cursive]">Sk'ask</h1>
+        <div className="flex bg-[#242424] gap-4 p-4 h-full custom-scrollbar2">
+          <form onSubmit={onsubmit} className="relative ">
+            <textarea
+              value={code}
+              onChange={(e) => setCode(e.target.value)}
+              name="code"
+              className=" custom-scrollbar2 w-[50vw] text-white rounded-r h-[95vh] p-4 pl-24 bg-[#0a0a0a] resize-none"
+            ></textarea>
+            <button
+              type="submit"
+              className=" cursor-pointer absolute bottom-4 right-4 text-white"
+            >
+              <img src={arrow} className="bg-white rounded-3xl" alt="submit" />
+            </button>
+          </form>
 
-        <div className="bg-[#343434] w-[50vw] h-[95vh] overflow-auto custom-scrollbar2 rounded-r text-white p-4 pl-6">
-          {error ? (
-            <span className="text-red-700 text-center">{errorText}</span>
-          ) : loading ? (
-            <Loader2 />
-          ) : (
-            <pre className="whitespace-pre-wrap">{typeof response === "string"? response:JSON.stringify(response, null,2)}</pre>
-          )}
+          <div className="bg-[#343434] w-[50vw] h-[95vh] overflow-auto custom-scrollbar2 rounded-r text-white p-4 pl-6">
+            {error ? (
+              <span className="text-red-700 text-center">{errorText}</span>
+            ) : loading ? (
+              <Loader2 />
+            ) : (
+              <pre className="whitespace-pre-wrap">{typeof response === "string"? response:JSON.stringify(response, null,2)}</pre>
+            )}
+          </div>
         </div>
-      </div>
-    </>
-  );
-}
+      </>
+    );
+  }
 
-export default App;
+  export default App;
