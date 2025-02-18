22dfda0495b928a4ee3064283b5bc8cd3f7ec090
incrediblesaizan1 <glorioussaizan1@gmail.com>
2025-02-18 13:14:36 +0530

    s

--- frontend/src/function/axiosInstance.ts
+++ frontend/src/function/axiosInstance.ts
@@ -3,7 +3,7 @@ import axios from "axios";
 export const axiosInstance = axios.create({
   baseURL: "https://incrediblesaizan1-code-review-backend.vercel.app",
   // baseURL: "http://localhost:3000",
-  timeout: 20000,
+  timeout: 50000,
   withCredentials: true,
   headers: {
     "Content-Type": "application/json",
