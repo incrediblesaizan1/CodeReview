f385d13f318335804a32abed88a6bffd179f8052
incrediblesaizan1 <glorioussaizan1@gmail.com>
2025-02-18 12:47:23 +0530

    c

--- backend/src/app.js
+++ backend/src/app.js
@@ -3,33 +3,45 @@ const app = express();
 import {getData} from "./services/ai.services.js"
 import cors from "cors"
 
-const allowedOrigins = [
-    "http://localhost:5173",
-    "https://incrediblesaizan1-code-review.vercel.app",
-  ];
+// const allowedOrigins = [
+//     "http://localhost:5174",
+//     "https://incrediblesaizan1-code-review.vercel.app",
+//   ];
+
+  // app.use(
+  //     cors({
+  //       origin: function (origin, callback) {
+  //         if (!origin || allowedOrigins.includes(origin)) {
+  //           callback(null, true);
+  //         } else {
+  //           callback(new Error("Not allowed by CORS"));
+  //         }
+  //       },
+  //       credentials: true, 
+  //       methods: ["GET", "POST", "PUT", "DELETE", "OPTIONS"], 
+  //       allowedHeaders: ["Content-Type", "Authorization"], 
+  //     })
+  //   );
+
+  let whitelist = ['https://incrediblesaizan1-code-review.vercel.app', 'http://localhost:5174']
+  let corsOptions = {
+    origin: function (origin, callback) {
+      if (whitelist.indexOf(origin) !== -1) {
+        callback(null, true)
+      } else {
+        callback(new Error('Not allowed by CORS'))
+      }
+    }
+  }
 
 app.use(express.json());
-app.use(
-    cors({
-      origin: function (origin, callback) {
-        if (!origin || allowedOrigins.includes(origin)) {
-          callback(null, true);
-        } else {
-          callback(new Error("Not allowed by CORS"));
-        }
-      },
-      credentials: true, 
-      methods: ["GET", "POST", "PUT", "DELETE", "OPTIONS"], 
-      allowedHeaders: ["Content-Type", "Authorization"], 
-    })
-  );
 
 
-app.get("/", async(req, res) => {
+app.get("/",cors(corsOptions), async(req, res) => {
    res.send("hello")
 });
 
-app.post("/",async(req,res)=>{
+app.post("/",cors(corsOptions),async(req,res)=>{
     const question = req.body.code
     const data = await getData(question) 
     res.send(data)
--- frontend/src/function/axiosInstance.ts
+++ frontend/src/function/axiosInstance.ts
@@ -1,7 +1,8 @@
 import axios from "axios";
 
 export const axiosInstance = axios.create({
-  baseURL: "https://incrediblesaizan1-code-review-backend.vercel.app",
+  // baseURL: "https://incrediblesaizan1-code-review-backend.vercel.app",
+  baseURL: "http://localhost:3000",
   timeout: 20000,
   withCredentials: true,
   headers: {
